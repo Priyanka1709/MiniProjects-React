6db106c926e8f79c098a4005e3e71cad954f88b4
Priyanka1709 <priyanka.karolia17@gmail.com>
2024-06-26 15:14:40 +0530

    clean up

---
+++ DynamicBarChart/README.md
@@ -0,0 +1,39 @@
+# BarCharts
+
+This project is a simple, dynamic bar chart visualization built with React.
+![alt text](image.png)
+
+## Getting Started
+
+To get started with this project, clone the repository and install the dependencies:
+
+```bash
+git clone https://github.com/your-repo/BarCharts.git
+cd BarCharts
+npm install
+```
+
+## Running the Project
+
+You can start the development server with:
+
+```bash
+npm start
+```
+
+This will start the server on `localhost:3000`.
+
+You can build the project for production with:
+
+```bash
+npm run build
+```
+
+## Dependencies
+
+This project uses the following dependencies:
+
+- `react`: ^18.0.0
+- `react-dom`: ^18.0.0
+- `react-scripts`: ^5.0.0
+
---
+++ DynamicBarChart/src/App.js
@@ -0,0 +1,79 @@
+import { useEffect, useState, useMemo } from "react";
+import { getData } from "./data";
+// import Bar from "./Bar";
+
+function Bar({ item, highestTicket }) {
+  const height = (item.ticketCount / highestTicket) * 100;
+  return (
+    <div
+      className="bar"
+      style={{
+        height: `${height}%`,
+        "background-color": item.colour,
+      }}
+      title={`${item.name} (${item.ticketCount})`}
+    ></div>
+  );
+}
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
+  useEffect(() => {
+    async () => {
+      const response = await getData();
+      setData(response);
+      setOriginalData(response);
+    };
+  }, []);
+
+  const getHighestTicketCount = useMemo(() => {
+    const val = data?.reduce((acc, item) => {
+      if (item.ticketCount > acc) {
+        return item.ticketCount;
+      }
+      return acc;
+    }, 0);
+    return val;
+  }, [data]);
+
+  if (data.length === 0) {
+    return;
+  }
+
+  const highestTicket = getHighestTicketCount;
+  const handleSelectChange = (e) => {
+    const newData = [...data];
+
+    if (e.target.value === "ascending") {
+      const sortedData = newData.sort(
+        (item1, item2) => item1.ticketCount - item2.ticketCount
+      );
+      setData(sortedData);
+    } else if (e.target.value === "descending") {
+      const sortedData = newData.sort(
+        (item1, item2) => item2.ticketCount - item1.ticketCount
+      );
+      setData(sortedData);
+    } else {
+      setData(originalData);
+    }
+  };
+
+  return (
+    <>
+      <select name="Sort" id="sort" onChange={handleSelectChange}>
+        <option value="default">Default</option>
+        <option value="ascending">Ascending</option>
+        <option value="descending">Descending</option>
+      </select>
+      <div className="bar-container">
+        {data.map((item) => {
+          return (
+            <Bar key={item.id} item={item} highestTicket={highestTicket} />
+          );
+        })}
+      </div>
+    </>
+  );
+}
